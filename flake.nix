dd3ae01c65f11d26ce568b2c6894ae993463c6d8
Vladimir Timofeenko <id@vtimofeenko.com>
2022-06-26 19:07:41 -0700

    chore: bump nixpkgs to 22.05

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "Flake with the module containing my zsh config";
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
 
   outputs = { self, nixpkgs }: {
     nixosModule = import ./module.nix;
